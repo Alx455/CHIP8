5b718a5734784cc90144962a68dd87c101dd0aea
Alx455 <alexpacheco4455@gmail.com>
2024-07-23 15:01:52 -0700

    set up main function and SDL for display

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <SDL.h>
+#include "CHIP8.h"
+#include <iostream>
+
+const int WINDOW_WIDTH = 64;
+const int WINDOW_HEIGHT = 32;
+const int SCALE = 10; // Scale the CHIP-8 display to make it larger
+
+uint8_t keymap[16]{
+        SDLK_x,
+        SDLK_1,
+        SDLK_2,
+        SDLK_3,
+        SDLK_q,
+        SDLK_w,
+        SDLK_e,
+        SDLK_a,
+        SDLK_s,
+        SDLK_d,
+        SDLK_z,
+        SDLK_c,
+        SDLK_4,
+        SDLK_r,
+        SDLK_f,
+        SDLK_v,
+};
+
+int main(int argc, char* argv[]) {
+
+
+    return 0;
+}
