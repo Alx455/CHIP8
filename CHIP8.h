f9205ba2155f00a5c971d455c24d28992c55f162
Alx455 <alexpacheco4455@gmail.com>
2024-07-22 20:50:37 -0700

    Established header file

--- CHIP8.h
+++ CHIP8.h
@@ -1,13 +1,33 @@
 #ifndef CHIP8_H
 #define CHIP8_H
+
+#include <string>
+
 class CHIP8 {
 private:
+	uint16_t I;									// Index Register
+	uint16_t pc;								// Program Counter
+
+	uint8_t sp;									// Stack Pointer
+	uint16_t stack[16];							// 16 level Stack
+
+	uint8_t V[16];								// 16 registers V0-VF
+	uint8_t mem[4096];							// 4K Byte Memory
+
+	uint8_t soundTimer;							// Sound Timer
+	uint8_t delayTimer;							// Delay Timer
 
 public:
 	CHIP8();
 	~CHIP8();
 
+	bool drawFlag;								// Indicates need to update display
 
+	void loadGame(std::string gameFilePath);	// Loads a ROM file given a file path
+	void cycle();								// Emulate a single cycle
+
+	uint8_t display[64 * 32];					// Display
+	uint8_t keypad[16];							// Input keys
 
 };
 
